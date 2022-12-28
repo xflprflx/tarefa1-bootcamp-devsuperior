9bf8c3970af0c3276eb5a230d549ae9d4ec5796c
Filipe Machado <x.filipe.machado.x@gmail.com>
2022-12-28 20:43:16 -0300

    Backend project created

---
+++ backend/src/test/java/com/devsuperior/tarefa1/Tarefa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.tarefa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarefa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
