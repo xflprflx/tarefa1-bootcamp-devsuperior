3cbec8a5973daaee7627580c2659425f1a01c6dd
Filipe Machado <x.filipe.machado.x@gmail.com>
2022-12-31 10:29:58 -0300

    Find by id

--- backend/src/main/java/com/devsuperior/tarefa1/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/tarefa1/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.tarefa1.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,12 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 }
