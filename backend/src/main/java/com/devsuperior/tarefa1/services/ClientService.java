06159429891223280d1a170a68d119bd7d5d3320
Filipe Machado <x.filipe.machado.x@gmail.com>
2022-12-31 11:18:18 -0300

    exception handling

--- backend/src/main/java/com/devsuperior/tarefa1/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/tarefa1/services/ClientService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.tarefa1.dto.ClientDTO;
 import com.devsuperior.tarefa1.entities.Client;
 import com.devsuperior.tarefa1.repositories.ClientRepository;
+import com.devsuperior.tarefa1.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -27,7 +28,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.get();
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 }
